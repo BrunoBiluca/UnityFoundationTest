e00838225335a44fba195286272970fd6099cb53
Bruno <b.b.da.costa@gmail.com>
2021-10-18 07:36:54 -0300

    :sparkles: Melhoria na exibição dos shaders

--- Assets/Scenes/Shaders/shader_samples/Dissolve/DissolveTrigger.cs
+++ Assets/Scenes/Shaders/shader_samples/Dissolve/DissolveTrigger.cs
@@ -1,3 +1,4 @@
+using Assets.UnityFoundation.Code.TimeUtils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +6,38 @@ using UnityEngine.InputSystem;
 
 public class DissolveTrigger : MonoBehaviour
 {
+    [SerializeField] private bool loop;
+
     private SkinnedMeshRenderer rend;
     private float dissolveSpeed = 1f;
     private bool dissolveReverse = false;
 
+    private Timer dissolveLoopTimer;
+
     private void Awake()
     {
         rend = GetComponentInChildren<SkinnedMeshRenderer>();
+
+        if(loop)
+            dissolveLoopTimer = new Timer(3f, () => Render()).Loop().Start();
     }
 
     void Update()
     {
         if(Keyboard.current.dKey.wasPressedThisFrame)
         {
-            foreach(var mat in rend.materials)
-            {
-                mat.SetInt("_DissolveReverse", dissolveReverse ? 1 : 0);
-                mat.SetFloat("_DissolveSpeed", dissolveSpeed);
-                mat.SetFloat("_DissolveTime", Time.time);
-            }
-            dissolveReverse = !dissolveReverse;
+            Render();
         }
+    }
+
+    private void Render()
+    {
+        foreach(var mat in rend.materials)
+        {
+            mat.SetInt("_DissolveReverse", dissolveReverse ? 1 : 0);
+            mat.SetFloat("_DissolveSpeed", dissolveSpeed);
+            mat.SetFloat("_DissolveTime", Time.time);
+        }
+        dissolveReverse = !dissolveReverse;
     }
 }
